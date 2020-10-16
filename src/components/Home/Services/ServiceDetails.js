cc8aad836099c7c2a84dca11404c83c3b37c5d10
arpolash <Arpolas2019@gmail.com>
2020-10-16 09:59:19 +0600

    card hover effect added

--- src/components/Home/Services/ServiceDetails.js
+++ src/components/Home/Services/ServiceDetails.js
@@ -12,8 +12,8 @@ const ServiceDetails = ({service}) => {
     return ( 
      
         <div className="col-md-4 col-12 my-5">
-           <Link to='/dashboard' onClick={() => handleCourse(service.title)}>
-            <div className="card text-center  h-100 p-3 shadow">
+           <Link to='/dashboard' className="text-decoration-none" onClick={() => handleCourse(service.title)}>
+            <div className="card text-center h-100 p-3 card-effect shadow">
         <img  className="card-img w-25 mx-auto"  src={`data:image/png;base64,${service.image.img}`} alt="not found" />
         <div className="card-body">
           <h5 className="card-title">{service.title}</h5>
